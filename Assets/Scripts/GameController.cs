22e231f58c91ac6794904f1af8722ad79f57f928
calmdownman <cskagud7@naver.com>
2023-02-23 19:44:01 +0900

    벽 위치 설정 이해 못 함

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -14,6 +14,8 @@ public class GameController : MonoBehaviour
     [SerializeField]
     private int currentLevel = 1;
 
+    private readonly float wallMaxScaleY = 20;
+
     //레벨에 따른 벽 개수 [레벨-1] = 벽 개수
     private readonly int[] wallCountByLevel = new int[7] {1,2,3,4,5,6,7};
 
@@ -39,9 +41,24 @@ public class GameController : MonoBehaviour
             }
         }
 
+        //모든 벽 위치, 크기 설정
         for(int i = 0;i < numberOfWalls;i++)
         {
-
+            //벽 크기 설정 (y)
+            Vector3 scale = new Vector3(0.5f,1,1);
+            scale.y = wallMaxScaleY / numberOfWalls; //number:1,scale.y:20 , number:2,scale.y:10
+            //벽 위치 설정 (y)
+            Vector3 position = Vector3.zero;
+            position.y = scale.y * (numberOfWalls / 2-i) - (numberOfWalls % 2 == 0 ? scale.y / 2 : 0);
+            //왼,오른쪽 벽 위치/크기 설정
+            SetTransform(leftWalls.GetChild(i), position,scale);
+            SetTransform(rightWalls.GetChild(i), position,scale);
         }
+    }
+
+    private void SetTransform(Transform t, Vector3 position, Vector3 scale)
+    {
+        t.localPosition = position;
+        t.localScale = scale;
     }
 }
